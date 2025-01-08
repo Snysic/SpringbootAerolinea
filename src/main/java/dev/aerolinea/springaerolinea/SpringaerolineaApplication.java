eeedbbcfd8edb28e722ab9f0c79bff8c7f19f4f7
Maria Vasilenko <snysic1987@gmail.com>
2025-01-08 12:58:29 +0100

    Initial commit

---
+++ src/main/java/dev/aerolinea/springaerolinea/SpringaerolineaApplication.java
@@ -0,0 +1,13 @@
+package dev.aerolinea.springaerolinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaerolineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaerolineaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aerolinea/springaerolinea/SpringaerolineaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aerolinea.springaerolinea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaerolineaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
